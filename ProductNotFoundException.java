635efbd38f2e15b6576a75525b9c6755ddcb97eb
RattaphumFame <rattaphum.s@ku.th>
2025-08-08 15:30:37 +0700

    Delete ProductNotFoundException.java

--- ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package Lib;
-/**
- * จะโยนException ก็ต่อเมื่อไม่เจอสินค้า
- */
-public class ProductNotFoundException extends Exception{
-    public ProductNotFoundException(String message){
-        super(message);
-    }
-}
