7240bcb4f431e0446de5368786459ae1f4b30f3c
RattaphumFame <rattaphum.s@ku.th>
2025-08-08 15:42:45 +0700

    Delete Lib directory

--- Lib/InvaidOperationException.java
+++
@@ -1,9 +0,0 @@
-package Lib;
-/**
- * จะโยนException ก็ต่อเมื่อคืนค่าสินค้าไม่สำเร็จ
- */
-public class InvaidOperationException extends Exception {
-    public InvaidOperationException(String message){
-        super(message);
-    }
-}
